3973aa05a0f70a17f9d8bf5a5320d5f6cdd70ab1
senaerbek <senaerbek@hotmail.com>
2020-11-30 21:06:28 +0300

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+(function(){
+  $('#msbo').on('click', function(){
+    $('body').toggleClass('msb-x');
+  });
+}());
+
+$( "#progress-button" ).click(function() {
+  $("#progress-menu").toggleClass('hidden-xs');
+ });
